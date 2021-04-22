19c82a61f792ad1d59005a00efe2472a5c67e667
tkubrak <taras.kubrak@dataart.com>
2021-04-22 11:24:57 +0300

    generating urls

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,20 +11,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response("Tesst");
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
         return $this->render('article/show.html.twig', [
-            'title' => $slug,
+            'title' => ucwords(str_replace('-', ' ', $slug)),
             'comments' => ['Comment 1', 'Comment 2', 'Comment 3']
         ]);
     }
